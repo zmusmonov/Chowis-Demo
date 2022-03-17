7ed2ed93fc0744aec4fae6b3b49a8efff9c74493
ZiyoMukhammad <zm.usmonov@gmail.com>
2022-03-17 14:12:50 +0500

    Initial Commit

---
+++ Chowis Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chowis Demo
+//
+//  Created by ZiyoMukhammad Usmonov on 17/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
